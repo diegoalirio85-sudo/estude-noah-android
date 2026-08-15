86cc72b5f84c1e502a5ec0a4ae172b32f9687466
diegoalirio85-sudo <diego.alirio85@gmail.com>
2026-08-15 18:22:52 -0300

    build: add Apache POI HSLF support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,5 +50,6 @@ dependencies {
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("org.apache.poi:poi-scratchpad:5.5.1")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
