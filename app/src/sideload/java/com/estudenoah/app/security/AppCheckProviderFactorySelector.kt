c735f2e1652112eb3808a16dbf6c656b7fe82740
diegoalirio85-sudo <diego.alirio85@gmail.com>
2026-08-25 15:11:04 -0300

    feat: use Play Integrity for sideload builds

---
+++ app/src/sideload/java/com/estudenoah/app/security/AppCheckProviderFactorySelector.kt
@@ -0,0 +1,9 @@
+package com.estudenoah.app.security
+
+import com.google.firebase.appcheck.AppCheckProviderFactory
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
+
+internal object AppCheckProviderFactorySelector {
+    fun factory(): AppCheckProviderFactory = PlayIntegrityAppCheckProviderFactory.getInstance()
+}
+
